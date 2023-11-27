d2d7605482f81436375b90adab7263d2b367a489
Muhammad Ahmad Saeed <ahmadsaeed3220@gmail.com>
2023-11-27 23:16:27 +0500

    Add Get methods

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const app = express()
-const Movies = ["Interstellar","Oppenhiemer","Inception"]
+const movies = ["Interstellar","Oppenheimer","Inception", "Tenet"]
 
-app.get('/', function (req, res) {
-  res.send('Hello World this is Ahmad')
+app.get('/', function (req,res) {
+  res.send('Hey, Welcome to the Movie Mania')
 })
+
+app.get("/api/movies",function(req, res){
+  res.send(movies);
+});
+app.get("/api/movies/:index",function(req, res){
+  if(!movies[req.params.index])
+  {
+    return res.status(400).send("Movie not found.")
+  }
+  res.send(movies[req.params.index]);
+});
+
+
 
 app.listen(3000)
