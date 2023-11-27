9180bd1404322988117c101691560d61ad4ab077
Muhammad Ahmad Saeed <ahmadsaeed3220@gmail.com>
2023-11-27 23:59:50 +0500

    Add Comments

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ app.use(express.json());
 
 const movies = ["Interstellar","Oppenheimer","Inception", "Tenet"]
 
+// Welcome Page
 app.get('/', function (req,res) {
   res.send('Hey, Welcome to the Movie Mania')
 })
 
+// Get all movies
 app.get("/api/movies",function(req, res){
   res.send(movies);
 });
 
+// Get a specific movie at a specific index
 app.get("/api/movies/:index",function(req, res){
   if(!movies[req.params.index])
   {
@@ -20,16 +23,19 @@ app.get("/api/movies/:index",function(req, res){
   res.send(movies[req.params.index]);
 });
 
+// Update a specific index
 app.put("/api/movies/:index",function(req, res){
   movies[req.params.index]=req.body.name;
   res.send(movies[req.params.index]);
 });
 
+// Delete a specific index 
 app.delete("/api/movies/:index",function(req, res){
   movies.splice(req.params.index,1)
   res.send(movies);
 });
 
+// Add a specific index 
 app.post("/api/movies",function(req, res){
   movies.push(req.body.name);
   res.send(movies);
