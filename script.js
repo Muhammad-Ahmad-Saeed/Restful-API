657246504eabbf039e4d0f6ecb3f6225262361ab
Muhammad Ahmad Saeed <ahmadsaeed3220@gmail.com>
2023-11-27 23:29:40 +0500

    Add Put Method

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+app.use(express.json());
+
 const movies = ["Interstellar","Oppenheimer","Inception", "Tenet"]
 
 app.get('/', function (req,res) {
@@ -9,14 +11,18 @@ app.get('/', function (req,res) {
 app.get("/api/movies",function(req, res){
   res.send(movies);
 });
+
 app.get("/api/movies/:index",function(req, res){
   if(!movies[req.params.index])
   {
-    return res.status(400).send("Movie not found.")
+    return res.status(400).send("Movie not found.");
   }
   res.send(movies[req.params.index]);
 });
 
-
+app.put("/api/movies/:index",function(req, res){
+  movies[req.params.index]=req.body.name;
+  res.send(movies[req.params.index]);
+});
 
 app.listen(3000)
